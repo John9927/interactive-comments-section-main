7e466c8b595ddca64e35cf1df3362763067a6948
Giovanni Izzi <giovanni9972@libero.it>
2022-01-31 15:29:42 +0100

    add card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CardComponent } from './components/home/card/card.component';
+import { AddCardComponent } from './components/home/add-card/add-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    AddCardComponent
   ],
   imports: [
     BrowserModule,
